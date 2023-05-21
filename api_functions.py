44a97beefd12cc28e9352c672a452b43489f6dc6
cryptarbitrage-code <cryptosheetsharing+github@gmail.com>
2023-05-21 01:30:29 +0100

    add api_functions.py, settings.py

---
+++ api_functions.py
@@ -0,0 +1,21 @@
+import json
+import requests
+from settings import api_exchange_address
+
+
+def get_volatility_index_data(currency, start_timestamp, end_timestamp, resolution):
+    url = "/api/v2/public/get_volatility_index_data"
+    parameters = {
+        'currency': currency,
+        'start_timestamp': start_timestamp,
+        'end_timestamp': end_timestamp,
+        'resolution': resolution,
+    }
+    # send HTTPS GET request
+    print(parameters)
+    json_response = requests.get((api_exchange_address + url + "?"), params=parameters)
+    response_dict = json.loads(json_response.content)
+    vol_index_data = response_dict["result"]
+    print(vol_index_data)
+
+    return vol_index_data
