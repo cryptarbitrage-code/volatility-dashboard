78698fd96930befd776b23abd279f88b26be6ae2
cryptarbitrage-code <cryptosheetsharing+github@gmail.com>
2023-05-22 01:38:01 +0100

    first working version

---
+++ functions.py
@@ -0,0 +1,90 @@
+from api_functions import get_volatility_index_data
+from datetime import datetime
+import plotly.graph_objects as go
+import pandas as pd
+
+
+# resolution of dvol data
+now = datetime.now()
+end_timestamp = round(datetime.timestamp(now) * 1000)
+year_milliseconds = 1000 * 60 * 60 * 24 * 365
+start_timestamp = end_timestamp - year_milliseconds
+dvol_resolution = 3600 * 24  # resolution of vol data in seconds, e.g. 1 hour = 3600
+
+def get_dvol_data():
+    # fetch the DVOL data from the api
+    btc_raw_data = get_volatility_index_data('BTC', start_timestamp, end_timestamp, dvol_resolution)
+    eth_raw_data = get_volatility_index_data('ETH', start_timestamp, end_timestamp, dvol_resolution)
+
+    # put btc data into a dataframe, add column names
+    columns = ['timestamp', 'open', 'high', 'low', 'close']
+    df = pd.DataFrame(btc_raw_data['data'], columns=columns)
+    df['date'] = pd.to_datetime(df['timestamp'], unit='ms')
+    df['range'] = df['open'] - df['close']
+    # eth dataframe
+    df_eth = pd.DataFrame(eth_raw_data['data'], columns=columns)
+    df_eth['date'] = pd.to_datetime(df_eth['timestamp'], unit='ms')
+    df_eth['range'] = df_eth['open'] - df_eth['close']
+
+    # vol stats
+    current_vol = df.iloc[-1]['close']  # the last row (current candle) updates when fresh data is pulled
+    current_vol_eth = df_eth.iloc[-1]['close']
+
+    # IV Rank
+    year_min = df['low'].min()
+    year_max = df['high'].max()
+    iv_rank = (current_vol - year_min) / (year_max - year_min) * 100
+    # eth
+    year_min_eth = df_eth['low'].min()
+    year_max_eth = df_eth['high'].max()
+    iv_rank_eth = (current_vol_eth - year_min_eth) / (year_max_eth - year_min_eth) * 100
+
+    # IV percentile
+    total_periods = len(df)
+    periods_lower = len(df[(df['close'] <= current_vol)])
+    iv_percentile = (periods_lower / total_periods) * 100
+    # eth
+    total_periods_eth = len(df_eth)
+    periods_lower_eth = len(df_eth[(df_eth['close'] <= current_vol_eth)])
+    iv_percentile_eth = (periods_lower_eth / total_periods_eth) * 100
+
+    # BTC DVOL chart
+    candles = go.Figure(
+        data=[
+            go.Candlestick(
+                x=df['date'],
+                open=df['open'],
+                high=df['high'],
+                low=df['low'],
+                close=df['close'],
+            )
+        ]
+    )
+    candles.update_layout(
+        height=500, template='plotly_dark',
+        title=f'BTC DVOL    High: {year_max}, Low: {year_min}, Current: {current_vol}'
+    )
+
+    # ETH DVOL chart
+    candles_eth = go.Figure(
+        data=[
+            go.Candlestick(
+                x=df_eth['date'],
+                open=df_eth['open'],
+                high=df_eth['high'],
+                low=df_eth['low'],
+                close=df_eth['close'],
+            )
+        ]
+    )
+    candles_eth.update_layout(
+        height=500, template='plotly_dark',
+        title=f'ETH DVOL    High: {year_max_eth}, Low: {year_min_eth}, Current: {current_vol_eth}'
+    )
+
+    print('current vol:', current_vol)
+    print('IVR, min, max:', iv_rank, year_min, year_max)
+    print('IVP, total periods, periods lower:', iv_percentile, total_periods, periods_lower)
+
+    return candles, iv_rank, iv_percentile, current_vol, year_min, year_max, \
+           candles_eth, iv_rank_eth, iv_percentile_eth, current_vol_eth, year_min_eth, year_max_eth
--- main.py
+++ main.py
@@ -1,2 +1,163 @@
 # Volatility Dashboard
+import dash
+from dash import dcc, html
+from dash.dependencies import Input, Output
+import dash_bootstrap_components as dbc
+import dash_daq as daq
+import pandas as pd
+import plotly.graph_objects as go
+from datetime import datetime
+from api_functions import get_volatility_index_data
+from functions import get_dvol_data
 
+
+# Initialize the app
+app = dash.Dash(title="Volatility Dashboard", external_stylesheets=[dbc.themes.DARKLY])
+
+btc_dvol_candles, btc_iv_rank, btc_iv_percentile, current_vol, year_min, year_max, \
+eth_dvol_candles, eth_iv_rank, eth_iv_percentile, eth_current_vol, eth_year_min, eth_year_max = get_dvol_data()
+
+# Define the app layout
+app.layout = dbc.Container([
+    dbc.Row([
+        dbc.Col([html.H1(children='Crypto Volatility Dashboard')])
+    ]),
+    dbc.Row([
+        dbc.Col([
+            html.Div(children='''
+            Historical and implied volatility statistics for bitcoin and ethereum.
+            '''),
+        ])
+    ]),
+    dbc.Row([
+        dbc.Col([
+            dbc.Button(
+                html.B("Refresh"),
+                color="info",
+                id="refresh_button",
+                className="mb-3",
+                style={'width': '100px'}
+            ),
+        ])
+    ]),
+    dbc.Row([
+        dbc.Col([
+            dbc.Row([dcc.Graph(id='btc_dvol_candles', figure=btc_dvol_candles)]),
+        ], width=10),
+        dbc.Col([
+            dbc.Row([
+                daq.Gauge(
+                    id='btc_iv_rank_gauge',
+                    color="#00cfbe",
+                    showCurrentValue=True,
+                    min=0,
+                    max=100,
+                    label={'label': 'IV Rank', 'style': {'font-size': '20px'}},
+                    scale={'custom': {
+                        '0': {'label': '0', 'style': {'font-size': '15px'}},
+                        '20': {'label': '20', 'style': {'font-size': '15px'}},
+                        '40': {'label': '40', 'style': {'font-size': '15px'}},
+                        '60': {'label': '60', 'style': {'font-size': '15px'}},
+                        '80': {'label': '80', 'style': {'font-size': '15px'}},
+                        '100': {'label': '100', 'style': {'font-size': '15px'}},
+                    }},
+                    value=btc_iv_rank,
+                    size=150,
+                )
+            ]),
+            dbc.Row([
+                daq.Gauge(
+                    id='btc_iv_percentile_gauge',
+                    color="#00cfbe",
+                    showCurrentValue=True,
+                    min=0,
+                    max=100,
+                    label={'label': 'IV Percentile', 'style': {'font-size': '20px'}},
+                    scale={'custom': {
+                        '0': {'label': '0', 'style': {'font-size': '15px'}},
+                        '20': {'label': '20', 'style': {'font-size': '15px'}},
+                        '40': {'label': '40', 'style': {'font-size': '15px'}},
+                        '60': {'label': '60', 'style': {'font-size': '15px'}},
+                        '80': {'label': '80', 'style': {'font-size': '15px'}},
+                        '100': {'label': '100', 'style': {'font-size': '15px'}},
+                    }},
+                    value=btc_iv_percentile,
+                    size=150,
+                )
+            ])
+        ], width=1)
+
+    ]),
+    dbc.Row([
+        dbc.Col([
+            dbc.Row([dcc.Graph(id='eth_dvol_candles', figure=eth_dvol_candles)]),
+        ], width=10),
+        dbc.Col([
+            dbc.Row([
+                daq.Gauge(
+                    id='eth_iv_rank_gauge',
+                    color="#00cfbe",
+                    showCurrentValue=True,
+                    min=0,
+                    max=100,
+                    label={'label': 'IV Rank', 'style': {'font-size': '15px'}},
+                    scale={'custom': {
+                        '0': {'label': '0', 'style': {'font-size': '15px'}},
+                        '20': {'label': '20', 'style': {'font-size': '15px'}},
+                        '40': {'label': '40', 'style': {'font-size': '15px'}},
+                        '60': {'label': '60', 'style': {'font-size': '15px'}},
+                        '80': {'label': '80', 'style': {'font-size': '15px'}},
+                        '100': {'label': '100', 'style': {'font-size': '15px'}},
+                    }},
+                    value=eth_iv_rank,
+                    size=150,
+                )
+            ]),
+            dbc.Row([
+                daq.Gauge(
+                    id='eth_iv_percentile_gauge',
+                    color="#00cfbe",
+                    showCurrentValue=True,
+                    min=0,
+                    max=100,
+                    label={'label': 'IV Percentile', 'style': {'font-size': '20px'}},
+                    scale={'custom': {
+                        '0': {'label': '0', 'style': {'font-size': '15px'}},
+                        '20': {'label': '20', 'style': {'font-size': '15px'}},
+                        '40': {'label': '40', 'style': {'font-size': '15px'}},
+                        '60': {'label': '60', 'style': {'font-size': '15px'}},
+                        '80': {'label': '80', 'style': {'font-size': '15px'}},
+                        '100': {'label': '100', 'style': {'font-size': '15px'}},
+                    }},
+                    value=eth_iv_percentile,
+                    size=150,
+                )
+            ])
+        ], width=1)
+
+    ]),
+
+], fluid=True)
+
+@app.callback(
+    [
+        Output('btc_dvol_candles', 'figure'),
+        Output('btc_iv_rank_gauge', 'value'),
+        Output('btc_iv_percentile_gauge', 'value'),
+        Output('eth_dvol_candles', 'figure'),
+        Output('eth_iv_rank_gauge', 'value'),
+        Output('eth_iv_percentile_gauge', 'value'),
+    ],
+    Input('refresh_button', 'n_clicks'),
+)
+def refresh_data(n_clicks):
+    print('button presses: ', n_clicks)
+
+    btc_dvol_candles, btc_iv_rank, btc_iv_percentile, current_vol, year_min, year_max, eth_dvol_candles, \
+    eth_iv_rank, eth_iv_percentile, eth_current_vol, eth_year_min, eth_year_max = get_dvol_data()
+
+    return btc_dvol_candles, btc_iv_rank, btc_iv_percentile, eth_dvol_candles, eth_iv_rank, eth_iv_percentile
+
+# Run the app
+if __name__ == '__main__':
+    app.run_server(debug=True)
